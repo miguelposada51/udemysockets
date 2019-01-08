563dd491e146b60932fe9d0bb432d3724769d515
miguelposada <miguelposada51@gmail.com>
2019-01-07 22:11:48 -0500

    socket io init

--- sockets/01-server/dist/classes/server.js
+++ sockets/01-server/dist/classes/server.js
@@ -5,13 +5,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var environment_1 = require("../global/environment");
+var socket_io_1 = __importDefault(require("socket.io"));
+var http_1 = __importDefault(require("http"));
 var Server = /** @class */ (function () {
     function Server() {
         this.app = express_1.default();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = socket_io_1.default(this.httpServer);
+        this.escucharSockets();
     }
+    Object.defineProperty(Server, "instance", {
+        get: function () {
+            return this._instance || (this._instance = new this());
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Server.prototype.escucharSockets = function () {
+        console.log('Escuchando Conexiones - sockets');
+        this.io.on('connection', function (cliente) {
+            console.log('cliente conectado');
+        });
+    };
     Server.prototype.start = function (callback) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     };
     return Server;
 }());
--- sockets/01-server/dist/index.js
+++ sockets/01-server/dist/index.js
@@ -7,7 +7,7 @@ var server_1 = __importDefault(require("./classes/server"));
 var router_1 = __importDefault(require("./routes/router"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var cors_1 = __importDefault(require("cors"));
-var server = new server_1.default();
+var server = server_1.default.instance;
 // bodyPArser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
