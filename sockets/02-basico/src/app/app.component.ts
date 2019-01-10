13dd40230d6fb4a42278a64424965f64890dfefe
Daniel_Pinillos <daniel.pinillos@simplexity.com.co>
2019-01-09 22:00:15 -0500

    servidor de sockets y sigleton

--- sockets/02-basico/src/app/app.component.ts
+++ sockets/02-basico/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'	
+})
+
 export class AppComponent {
   title = 'basico';
+
+  constructor( public wsServices: WebsocketService  ){
+
+  }
+
+ ngOnInit() {
+
+ }
+
 }
+
